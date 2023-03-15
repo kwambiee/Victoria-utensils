2676f78ddf33ae7ac14f71f86afb398eada45c12
hillarioh <okerioh@gmail.com>
2023-03-15 08:00:00 +0300

    Add brand and category model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do
+    Category.create!(
+        title: Faker::Appliance.equipment
+        description: Faker::Lorem.paragraph(sentence_count: 2)
+    )
+end
+
+5.times do
+    Brand.create!(
+        name: Faker::Appliance.brand
+    )
+end
---
+++ spec/factories/categories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :category do
+    title { "MyString" }
+    description { "My Description" }
+  end
+end
